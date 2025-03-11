d606589a21e3eaf64a394316287aee46449c71e5
allanXQ <allancjuma@gmail.com>
2025-03-11 23:51:55 +0300

    update

--- server/controllers/daraja/c2b/confirmationUrl.js
+++ server/controllers/daraja/c2b/confirmationUrl.js
@@ -1,7 +1,7 @@
 const confirmationUrl = (req, res) => {
   try {
     console.log(req.body);
-    res.json({ message: "Confirmation URL received" });
+    res.json({ message: req.body });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error receiving confirmation URL" });
