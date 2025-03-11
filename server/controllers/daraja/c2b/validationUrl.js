64c66e19c8197f539fd4367f2073fabf101ecc2d
allanXQ <allancjuma@gmail.com>
2025-03-12 00:32:49 +0300

    update

--- server/controllers/daraja/c2b/validationUrl.js
+++ server/controllers/daraja/c2b/validationUrl.js
@@ -1,17 +1,9 @@
 const validationUrl = async (req, res) => {
-  try {
-    console.log(req.body);
-    res.json({
-      ResultCode: "0",
-      ResultDesc: "Accepted",
-    });
-  } catch (error) {
-    console.error(error);
-    res.json({
-      ResultCode: "0",
-      ResultDesc: "Accepted",
-    });
-  }
+  console.log(req.body);
+  return res.json({
+    ResultCode: "0",
+    ResultDesc: "Accepted",
+  });
 };
 
 module.exports = validationUrl;
