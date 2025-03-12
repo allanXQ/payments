097d948368a70af98da77777edbada0203a1a6a2
allanXQ <allancjuma@gmail.com>
2025-03-12 21:19:19 +0300

    update test

--- server/controllers/daraja/c2b/confirmationUrl.js
+++ server/controllers/daraja/c2b/confirmationUrl.js
@@ -1,6 +1,14 @@
-const confirmationUrl = (req, res) => {
+const axios = require("axios");
+
+const confirmationUrl = async (req, res) => {
   try {
     console.log(req.body);
+    await axios.post(
+      "https://c007-102-214-72-6.ngrok-free.app/api/v1/daraja/c2b-validation-url",
+      {
+        message: req.body,
+      }
+    );
     res.json({ message: req.body });
   } catch (error) {
     console.error(error);
