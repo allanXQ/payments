7d210910ae5c16b92b02f4118214337c859ab737
allanXQ <allancjuma@gmail.com>
2025-03-13 21:19:27 +0300

    update time format

--- server/controllers/daraja/c2b/confirmationUrl.js
+++ server/controllers/daraja/c2b/confirmationUrl.js
@@ -12,16 +12,31 @@ const confirmationUrl = async (req, res) => {
       BillRefNumber,
     } = req.body;
     // 20250312213418
-    const transactionTime = `${TransTime.slice(0, 4)}-${TransTime.slice(
-      4,
-      6
-    )}-${TransTime.slice(6, 8)
-      .concat(" ")
-      .concat(TransTime.slice(8, 10))
-      .concat(":")
-      .concat(TransTime.slice(10, 12))
-      .concat(":")
-      .concat(TransTime.slice(12, 14))}`;
+    const formatDate = (dateString) => {
+      const year = dateString.substring(0, 4);
+      const month = dateString.substring(4, 6);
+      const day = dateString.substring(6, 8);
+      const hours = dateString.substring(8, 10);
+      const minutes = dateString.substring(10, 12);
+      const seconds = dateString.substring(12, 14);
+
+      const dateObj = new Date(
+        `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`
+      );
+
+      return dateObj.toLocaleString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true,
+      });
+    };
+
+    const transactionTime = formatDate(TransTime);
     const message = `Sender first name: ${FirstName}
     Amount: ${TransAmount}
     Transaction ID: ${TransID}
