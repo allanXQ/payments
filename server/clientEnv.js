4181055a612f671181d64bd64ac5f18e82743268
allanXQ <allancjuma@gmail.com>
2025-03-22 12:12:19 +0300

    UPDATE prod server

--- server/clientEnv.js
+++ server/clientEnv.js
@@ -6,7 +6,7 @@ const writeClientEnv = () => {
   const urlpath = "/api/v1";
   const VITE_ENV = process.env.ENV;
   const VITE_DEV_SERVER_URL = process.env.DEV_SERVER_URL + urlpath;
-  const VITE_PROD_SERVER_URL = process.env.SERVER_URL + urlpath;
+  const VITE_PROD_SERVER_URL = process.env.PROD_SERVER_URL + urlpath;
 
   fs.writeFileSync(
     path.join(__dirname, "../client/.env"),
