7473593b63c921e93d49d83d8ca5c2585267aabd
allanXQ <allancjuma@gmail.com>
2025-03-23 00:28:20 +0300

    test2

--- server/controllers/daraja/b2c/b2cresulturl.js
+++ server/controllers/daraja/b2c/b2cresulturl.js
@@ -5,26 +5,26 @@ const B2CResultURL = async (req, res) => {
   try {
     const { Result } = req.body;
     console.log("Result", Result);
-    // const receiverEntry = Result.ResultParameters.ResultParameter.find(
-    //   (item) => item.Key === "ReceiverPartyPublicName"
-    // );
-    // const dateEntry = Result.ResultParameters.ResultParameter.find(
-    //   (item) => item.Key === "TransactionCompletedDateTime"
-    // );
+    const receiverEntry = Result.ResultParameters.ResultParameter.find(
+      (item) => item.Key === "ReceiverPartyPublicName"
+    );
+    const dateEntry = Result.ResultParameters.ResultParameter.find(
+      (item) => item.Key === "TransactionCompletedDateTime"
+    );
 
-    // const receiverName = receiverEntry
-    //   ? receiverEntry.Value.split(" - ")[1]
-    //   : null;
-    // const completionDate = dateEntry ? dateEntry.Value : null;
+    const receiverName = receiverEntry
+      ? receiverEntry.Value.split(" - ")[1]
+      : null;
+    const completionDate = dateEntry ? dateEntry.Value : null;
 
-    // await Transactions.updateOne(
-    //   { ConversationID: Result.ConversationID },
-    //   {
-    //     ReceiverPartyPublicName: receiverName,
-    //     TransactionCompletedDateTime: completionDate,
-    //     Status: "funded",
-    //   }
-    // );
+    await Transactions.updateOne(
+      { ConversationID: Result.ConversationID },
+      {
+        ReceiverPartyPublicName: receiverName,
+        TransactionCompletedDateTime: completionDate,
+        Status: "funded",
+      }
+    );
     return null;
   } catch (error) {
     logger.error(error);
